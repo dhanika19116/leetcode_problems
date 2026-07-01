b5ee13f6eb701b3f360c8ab50b7fd2bb9e2a0e35
dhanika19116 <dhanika19116@gmail.com>
2026-07-01 13:22:09 +0530

    Add files via upload

---
+++ stringrev.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def reverseWords(self, s):
+        l=s.split()
+        l2=[]
+        for i in l[::-1]:
+            l2.append(i)
+        s1=" ".join(l2)
+        return s1
